7637c3a00f42f857e3a4117b2f4504af41200a7b
VavRe <vahyd@live.com>
2021-04-15 04:26:46 +0430

    =First Setup for webpack+tailwindcss+postCSS is ready to go!

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const mode = process.env.NODE_ENV;
+
+const config = {
+  mode: mode,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          mode === "development" ? 'style-loader' :  MiniCssExtractPlugin.loader
+          ,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ]
+}
+
+module.exports = config
